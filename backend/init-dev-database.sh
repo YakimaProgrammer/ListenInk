3207b6b40fe2a1906847399f016b666083b12941
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2025-01-13 08:58:34 -0800

    Added an optional `--if-missing` flag to the dev db init script

--- backend/init-dev-database.sh
+++ backend/init-dev-database.sh
@@ -1,3 +1,20 @@
-rm prisma/dev.db
+#! /bin/bash
+
+if_missing=false
+for arg in "$@"; do
+    if [[ "$arg" == "--if-missing" ]]; then
+	if_missing=true
+    fi
+done
+
+# Main logic
+if [ "$if_missing" = true ]; then
+    if [ -f "prisma/dev.db" ]; then
+	#DB exists
+	exit 0
+    fi
+fi
+
+rm -f prisma/dev.db
 npx prisma migrate dev --name init
 npx tsx prisma/init-dev-database.ts
