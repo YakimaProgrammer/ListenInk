4e9101f209f16332c825a6c3e8e2493e7f4fb9a9
tario-you <tario01pd2024@gmail.com>
2025-01-22 02:35:16 -0800

    created templates

--- listenink/src/components/auth/Register.js
+++ listenink/src/components/auth/Register.js
@@ -1,7 +1,8 @@
-li {
-    width: 200px;
-    white - space: nowrap;
-    overflow: hidden;
-    text - overflow: ellipsis;
-    display: inline - block; /* or inline-flex/block depending on layout */
+import React from "react";
+
+export function Register() {
+    return (
+        <div>
+        </div>
+    );
 }
