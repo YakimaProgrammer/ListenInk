cbc167f611850ef08e37d14c82a7129e6db97f78
tario-you <tario01pd2024@gmail.com>
2025-01-28 08:44:39 -0800

    chat_context

---
+++ listenink/src/chat_context.py
@@ -0,0 +1,22 @@
+import os
+
+folder_path = "src/components/"
+
+# include = ["MainApp", "CategoriesContext", "categoriesSlice", "store"]
+exclude = [".DS_Store"]
+
+
+for root, _, files in os.walk(folder_path):
+    for file_name in files:
+        # any(i in file_name for i in include)
+        if not any(i in file_name for i in exclude):
+            file_path = os.path.join(root, file_name)
+            print(f"File: {file_path}\n```")
+            try:
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+                    print(f"Content:\n{content}\n")
+            except Exception as e:
+                # print(f"Could not read file {file_path} due to: {e}\n")
+                pass
+            print("```")
