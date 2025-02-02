8f643ff5767dcaba2dbc060a2e1c3c5b0c03a23e
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2025-02-02 13:38:08 -0800

    Material UI time!

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { StyledEngineProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+// Components
 import { Root } from "./pages/Root";
 import { Auth } from "./pages/Auth";
 
 // A global css file, but we use css modules for this project for namespacing
 import "./index.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { Provider } from "react-redux";
-import { store } from "./store";
 
 let router = createBrowserRouter([
   {
@@ -26,8 +29,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <StyledEngineProvider injectFirst>
+      <Provider store={store}>
+	<RouterProvider router={router} />
+      </Provider>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
--- frontend/src/pages/Root/index.tsx
+++ frontend/src/pages/Root/index.tsx
@@ -1,28 +1,128 @@
-import { connect } from "react-redux";
-import { Sidebar } from "../../components/Sidebar";
-import { RootState } from "../../store";
-import style from "./index.module.scss";
+import { connect, ConnectedProps } from "react-redux";
+import { AppDispatch, RootState, setSidebar } from "../../store";
+import { Drawer, IconButton, Divider, List, ListItem, ListItemText, ListItemButton, ListItemIcon } from '@mui/material';
+import { ChevronLeft, Inbox, Mail }  from '@mui/icons-material';
+import { styled } from '@mui/material/styles';
 
-interface RootProps {
-  sidebarOpen: boolean
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import styles from './index.module.scss';
+
+const mapStateToProps = (state: RootState) => ({
+  sidebarOpen: state.ui.sidebarOpen,
+});
+
+const mapDispatchToProps = (dispatch: AppDispatch) => ({
+  setOpen: () => dispatch(setSidebar(true)),
+  setClosed: () => dispatch(setSidebar(false))
+});
+
+// Get inferred props from connect()
+const connector = connect(mapStateToProps, mapDispatchToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
 }
 
-function RootComponent({ sidebarOpen }: RootProps) {
-  const pageStyle = sidebarOpen ? style.page : `${style.page} ${style.sidebarHidden}`;
+const AppBar = ({ open, ...props }: AppBarProps) => (
+  <MuiAppBar
+    {...props}
+    className={`${styles.appBar} ${open ? styles.appBarOpen : ''}`}
+  />
+);
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+
+function RootComponent({ sidebarOpen, setOpen, setClosed }: PropsFromRedux) {
+  const theme = useTheme();
   
   return (
-    <div className={pageStyle}>
-      {/* Sidebar */}
-      <div className={style.sidebar}>
-	<Sidebar />
-      </div>
-
-      {/* Main panel */}
-      <div className={style.main}>
-
-      </div>
-    </div>
+    <Box className={styles.root}>
+      <CssBaseline />
+      <AppBar position="fixed" open={sidebarOpen}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={setOpen}
+            edge="start"
+            className={`${styles.menuButton} ${sidebarOpen ? styles.hidden : ''}`}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={styles.drawer}
+        variant="persistent"
+        anchor="left"
+        open={sidebarOpen}
+        classes={{ paper: styles.drawerPaper }}
+      >
+        <DrawerHeader>
+          <IconButton onClick={setClosed}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={`${styles.main} ${sidebarOpen ? styles.mainOpen : ''}`}>
+        <DrawerHeader />
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+        </Typography>
+      </main>
+</Box>
   );
 }
 
-export const Root = connect((state: RootState) => ({ sidebarOpen: state.ui.sidebarOpen }))(RootComponent);
+export const Root = connector(RootComponent);
