77e13aa223a4252198817991e56839aba1e0f824
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2025-02-23 05:44:11 -0800

    Switched to stricter schema verification

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,35 +1,34 @@
 import { z } from "zod";
 
 export const DocIdSchema = z.object({
-  document_id: z.string(),
+  document_id: z.string().nanoid(),
 });
 export type DocId = z.infer<typeof DocIdSchema>;
 
 export const CategorySchema = z.object({
-  userId: z.string(),
+  userId: z.string().nanoid(),
   color: z.string(),
   name: z.string(),
-  id: z.string(),
+  id: z.string().nanoid(),
 });
 export type Category = z.infer<typeof CategorySchema>;
 
 export const BookmarkSchema = z.object({
-  id: z.string(),
-  page: z.number(),
-  audiotime: z.number(),
-  documentId: z.string(),
+  id: z.string().nanoid(),
+  page: z.number().finite().positive(),
+  audiotime: z.number().finite().positive(),
+  documentId: z.string().nanoid(),
 });
 export type Bookmark = z.infer<typeof BookmarkSchema>;
 
 export const DocumentSchema = z.object({
   name: z.string(),
-  numpages: z.number(),
+  numpages: z.number().finite().positive(),
   s3key: z.string(),
   bookmarks: z.array(BookmarkSchema),
-  // shares: z.array(z.string()), // Uncomment if needed
-  id: z.string(),
+  id: z.string().nanoid(),
   completed: z.boolean(),
-  categoryId: z.string()
+  categoryId: z.string().nanoid()
 });
 export type Document = z.infer<typeof DocumentSchema>;
 
@@ -40,7 +39,7 @@ export type Err = z.infer<typeof ErrSchema>;
 
 export const UserSchema = z.object({
   name: z.string(),
-  id: z.string(),
-  email: z.string()
+  id: z.string().nanoid(),
+  email: z.string().email()
 });
 export type User = z.infer<typeof UserSchema>;
